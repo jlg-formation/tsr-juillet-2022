aed9654edb15f39a3fc83f4e00e690b22a24e115
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-07-07 10:47:18 +0200

    dev: circles samples ok

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ let truc = 4;
 console.log("truc: ", truc);
 
 const svgns = "http://www.w3.org/2000/svg";
+const r0 = 45;
+const x0 = 50;
+const y0 = 50;
 
 const sampleGroup = document.querySelector("svg g.samples");
 
 const sampleNbr = 10;
 
 for (let i = 0; i < sampleNbr; i++) {
+  const angle = (i * 2 * Math.PI) / sampleNbr;
   const r = 1;
-  const x = 34;
-  const y = 45;
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
