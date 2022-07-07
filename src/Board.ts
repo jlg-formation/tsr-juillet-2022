eedd26c51403931d5fe3580e984ee613781383ad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-07-07 14:29:00 +0200

    refactor: to ts

---
+++ src/Board.ts
@@ -0,0 +1,38 @@
+interface BoardConfig {
+  sampleNbr: number;
+}
+
+const SVGNS = "http://www.w3.org/2000/svg";
+const r0 = 45;
+const x0 = 50;
+const y0 = 50;
+
+class Board {
+  config: BoardConfig;
+
+  constructor() {}
+
+  draw() {
+    const sampleGroup = document.querySelector("svg g.samples");
+    if (sampleGroup === null) {
+      throw new Error("element with selector 'svg g.samples' not found.");
+    }
+
+    for (let i = 0; i < this.config.sampleNbr; i++) {
+      const angle = (i * 2 * Math.PI) / this.config.sampleNbr;
+      const r = 1;
+      const x = x0 + r0 * Math.cos(angle);
+      const y = y0 + r0 * Math.sin(angle);
+
+      const circle = document.createElementNS(SVGNS, "circle");
+      circle.setAttributeNS(null, "cx", String(x));
+      circle.setAttributeNS(null, "cy", y.toString());
+      circle.setAttributeNS(null, "r", r + "");
+      sampleGroup.appendChild(circle);
+    }
+  }
+
+  setConfig(config: BoardConfig) {
+    this.config = config;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
-let truc = 4;
-console.log("truc: ", truc);
-
-const svgns = "http://www.w3.org/2000/svg";
-const r0 = 45;
-const x0 = 50;
-const y0 = 50;
-
-const sampleGroup = document.querySelector("svg g.samples");
-if (sampleGroup === null) {
-  throw new Error("element with selector 'svg g.samples' not found.");
-}
-const sampleNbr = 10;
-
-for (let i = 0; i < sampleNbr; i++) {
-  const angle = (i * 2 * Math.PI) / sampleNbr;
-  const r = 1;
-  const x = x0 + r0 * Math.cos(angle);
-  const y = y0 + r0 * Math.sin(angle);
-
-  const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", String(x));
-  circle.setAttributeNS(null, "cy", y.toString());
-  circle.setAttributeNS(null, "r", r + "");
-  sampleGroup.appendChild(circle);
-}
+const board = new Board();
+board.setConfig({
+  sampleNbr: 10,
+});
+board.draw();
