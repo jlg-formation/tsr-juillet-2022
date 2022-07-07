c764567f6c6a3bdf655cb0cfe30fc27f40f0e4ab
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-07-07 17:35:55 +0200

    dev: added webpack target and review config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,60 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { config } = require("process");
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
-  ],
-  mode: "development",
-  entry: "./src/main.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
+module.exports = (_, args) => {
+  console.log("args: ", args);
+  const mode = args.mode ?? "development";
+  console.log("mode: ", mode);
+  const config = {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+    mode,
+    entry: "./src/main.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
     },
-    compress: true,
-    port: 9000,
-  },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      compress: true,
+      port: 9000,
+    },
+  };
+
+  if (mode === "development") {
+    config.devtool = "inline-source-map";
+  }
+  return config;
 };
