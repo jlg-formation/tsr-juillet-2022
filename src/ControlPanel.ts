3c584b3d5d4aac79b5a3320e28646e1a177d98ff
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-07-08 12:04:52 +0200

    dev: action and state button

--- src/ControlPanel.ts
+++ src/ControlPanel.ts
@@ -4,8 +4,32 @@ import { $, $$ } from "./misc";
 type CallBackFn = (config: BoardConfig) => void;
 
 export class ControlPanel {
+  _isPlaying = false;
   callback: CallBackFn = () => {};
+
   constructor(private config: BoardConfig) {
+    this.manageSliders();
+    this.managePlayButton();
+    this.redraw();
+  }
+
+  set isPlaying(val: boolean) {
+    this._isPlaying = val;
+    this.redraw();
+  }
+
+  get isPlaying() {
+    return this._isPlaying;
+  }
+
+  managePlayButton() {
+    const btn = $(`div.control-panel button[title="Play"]`);
+    btn.addEventListener("click", () => {
+      this.isPlaying = !this.isPlaying;
+    });
+  }
+
+  manageSliders() {
     const array: (keyof BoardConfig)[] = ["sampleNbr", "multiplier"];
     array.forEach((key) => {
       const span = $(`div.control-panel label.${key} span`);
@@ -23,6 +47,11 @@ export class ControlPanel {
         this.callback(this.config);
       });
     });
+  }
+
+  redraw() {
+    const btn = $(`div.control-panel button[title="Play"]`);
+    btn.innerHTML = this.isPlaying ? "Stop" : "Play";
   }
 
   subscribe(callback: CallBackFn) {
